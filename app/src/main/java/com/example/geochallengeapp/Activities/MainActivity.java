c0de182d9646231db7489c52b405e39b90c5ad8b
Nir <nirose@gmail.com>
2020-04-14 19:12:26 +0300

    added - lambda fetch server ip code compact

--- app/src/main/java/com/example/geochallengeapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/geochallengeapp/Activities/MainActivity.java
@@ -6,8 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.transition.TransitionManager;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -19,41 +17,22 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MAIN";
 
-    private ViewGroup transitionsContainer;
-    private TextView tv_play;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        transitionsContainer = findViewById(R.id.activity_main);
+        ViewGroup transitionsContainer = findViewById(R.id.activity_main);
         transitionsContainer.setOnClickListener((v)-> {
             FireIntent(SetupActivity.class);
         });
 
-        tv_play = transitionsContainer.findViewById(R.id.tv_play);
+        TextView tv_play = transitionsContainer.findViewById(R.id.tv_play);
         TransitionManager.beginDelayedTransition(transitionsContainer);
         tv_play.setTextSize(50);
         tv_play.setVisibility(View.VISIBLE);
 
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        return super.onOptionsItemSelected(item);
     }
 
     private void FireIntent(Class cls) {
