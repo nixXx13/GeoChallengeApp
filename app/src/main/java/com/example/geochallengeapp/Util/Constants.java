a64136987b39f0f71e27df80710ee8a89becd5d4
Nir <nirose@gmail.com>
2020-06-06 16:58:13 +0300

    constants

--- app/src/main/java/com/example/geochallengeapp/Util/Constants.java
+++ app/src/main/java/com/example/geochallengeapp/Util/Constants.java
@@ -9,4 +9,11 @@ public class Constants {
     public static final String SCORE_TEMPLATE = "Score: %.2f";
     public static final String NO_ANSWER = "NO_ANSWER";
 
+
+    public final static int SERVER_PORT = 4567;
+    public static final String IP_LAMBDA = "";
+    public static final String QUESTION_PROVIDER_DOCKER_IP = "172.18.0.2";
+    public static final int QUESTION_PROVIDER_DOCKER_PORT = 500;
+
+
 }
